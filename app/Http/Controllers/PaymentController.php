581c059e89f85c86e760460dd9f905ca93326f22
luckykun88 <kmisigaro@outlook.com>
2022-07-25 18:41:29 +0300

    Changes

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -27,8 +27,50 @@ class PaymentController extends Controller
             'payed' => !$applicant->payed
         ]);
 
-        $recieptController = new RecieptController();
-        $recieptController->createReceipt($id);
+        $client = new Party([
+            'name'          => 'Paediatric Assosication of Tanzania',
+            'phone'         => '+255754047857',
+        ]);
+
+        $customer = new Party([
+            'name'          =>  $payment->user->name,
+            'phone'         =>  $payment->user->phone
+        ]);
+
+        $items = [
+            (new InvoiceItem())
+                ->title('Payment of attendance')
+                ->description('Your product or service description')
+                ->pricePerUnit($payment->amount)
+                ->quantity(1)
+        ];
+
+        $notes = [
+            'You can use this receipt to present to PAT as a confirmation of payment',
+        ];
+        $notes = implode("<br>", $notes);
+
+        $invoice = Invoice::make('receipt')
+        // ability to include translated invoice status
+        // in case it was paid
+        ->status(__('invoices::invoice.paid'))
+        ->sequence(1)
+        ->seller($client)
+        ->buyer($customer)
+        ->date(now())
+        ->dateFormat('m/d/Y')
+        ->payUntilDays(1)
+        ->currencySymbol('Tshs')
+        ->currencyCode('TZS')
+        ->currencyFormat('{SYMBOL}{VALUE}')
+        ->currencyThousandsSeparator('.')
+        ->currencyDecimalPoint(',')
+        ->filename($customer->name)
+        ->addItems($items)
+        ->notes($notes)
+        // ->logo(public_path('patlogo.png'))
+        // You can additionally save generated invoice to configured disk
+        ->save('public');
 
         $name = $payment->user->name;
 
