4fd82fb0367b66491bfdd8fb8ac8c0a9d479b5d0
luckykun88 <kmisigaro@outlook.com>
2022-07-25 18:38:14 +0300

    Changes

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        LaravelDaily\Invoices\InvoiceServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -229,6 +230,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Invoice' => LaravelDaily\Invoices\Facades\Invoice::class,
 
     ],
 
